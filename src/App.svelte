c92c9dab7ddbe6c1d1db4b1768cdd5ba5a177a07
Luis Gaspar <luiscrg@gmail.com>
2026-05-12 09:01:51 +0100

    Clear main branch contents

--- src/App.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  import Calculator from '$lib/components/Calculator.svelte';
-</script>
-
-<main class="app">
-  <Calculator />
-</main>
--- src/app.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="svelte" />
-
-declare module '*.svelte' {
-  const component: any;
-  export default component;
-}
